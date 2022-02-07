6b0262a569c88c079b3eea75636d415a79e05d94
unknown <zanattasebas@gmail.com>
2022-02-07 17:31:19 -0300

    JSIV terminado!

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -123,6 +123,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for ( var i = 0; i < usuarios.length; i++){
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -132,6 +136,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0 ;
+  for (var i = 0 ; i < usuario.posts.length; i++){
+    suma = suma + usuario.posts[i].likes ;
+  }
+  return suma;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -144,7 +153,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function(){
+    return producto.precio - (producto.precio * producto.porcentajeDeDescuento) ; 
+  } 
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
